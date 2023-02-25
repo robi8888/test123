8d115aa673f2d4b12eaa124edf592db7af5f76c7
Robert Tušek <rotusek@vsite.hr>
2023-02-25 18:17:52 +0100

    Add project files.

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.broj1 = new System.Windows.Forms.TextBox();
+            this.broj2 = new System.Windows.Forms.TextBox();
+            this.broj3 = new System.Windows.Forms.TextBox();
+            this.broj4 = new System.Windows.Forms.TextBox();
+            this.broj5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dodatni = new System.Windows.Forms.TextBox();
+            this.broj = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Izvuci";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. broj";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. broj";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3. broj";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4. broj";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "5. broj";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(367, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "LOTTO";
+            // 
+            // broj1
+            // 
+            this.broj1.Location = new System.Drawing.Point(103, 42);
+            this.broj1.Name = "broj1";
+            this.broj1.Size = new System.Drawing.Size(38, 22);
+            this.broj1.TabIndex = 7;
+            // 
+            // broj2
+            // 
+            this.broj2.Location = new System.Drawing.Point(103, 85);
+            this.broj2.Name = "broj2";
+            this.broj2.Size = new System.Drawing.Size(38, 22);
+            this.broj2.TabIndex = 8;
+            // 
+            // broj3
+            // 
+            this.broj3.Location = new System.Drawing.Point(103, 121);
+            this.broj3.Name = "broj3";
+            this.broj3.Size = new System.Drawing.Size(38, 22);
+            this.broj3.TabIndex = 9;
+            // 
+            // broj4
+            // 
+            this.broj4.Location = new System.Drawing.Point(103, 162);
+            this.broj4.Name = "broj4";
+            this.broj4.Size = new System.Drawing.Size(38, 22);
+            this.broj4.TabIndex = 10;
+            // 
+            // broj5
+            // 
+            this.broj5.Location = new System.Drawing.Point(103, 201);
+            this.broj5.Name = "broj5";
+            this.broj5.Size = new System.Drawing.Size(38, 22);
+            this.broj5.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Dodatni broj";
+            // 
+            // dodatni
+            // 
+            this.dodatni.Location = new System.Drawing.Point(12, 295);
+            this.dodatni.Name = "dodatni";
+            this.dodatni.Size = new System.Drawing.Size(43, 22);
+            this.dodatni.TabIndex = 13;
+            // 
+            // broj
+            // 
+            this.broj.Location = new System.Drawing.Point(370, 145);
+            this.broj.Name = "broj";
+            this.broj.ReadOnly = true;
+            this.broj.Size = new System.Drawing.Size(245, 22);
+            this.broj.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(370, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Izvučeni brojevi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(675, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 41);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Izlaz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(614, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 60);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.broj);
+            this.Controls.Add(this.dodatni);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.broj5);
+            this.Controls.Add(this.broj4);
+            this.Controls.Add(this.broj3);
+            this.Controls.Add(this.broj2);
+            this.Controls.Add(this.broj1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox broj1;
+        private System.Windows.Forms.TextBox broj2;
+        private System.Windows.Forms.TextBox broj3;
+        private System.Windows.Forms.TextBox broj4;
+        private System.Windows.Forms.TextBox broj5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox dodatni;
+        private System.Windows.Forms.TextBox broj;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random numbers = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int broj = numbers.Next();
+            
+            if (broj1.Text == "")
+            {
+                broj1.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+            }
+                
+            
+            if (broj1.Text != "" && broj2.Text == "")
+            {
+
+                broj2.Text = Convert.ToString(numbers.Next(1, 37));
+                if (broj2.Text == broj1.Text)
+                    broj2.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+                
+            }
+
+            if (broj1.Text !="" && broj2.Text != "" && broj3.Text == "")
+            {
+                broj3.Text = Convert.ToString(numbers.Next(1, 37));
+                if (broj3.Text == broj1.Text || broj3.Text == broj2.Text)
+                    broj3.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+            }
+            if (broj1.Text != "" && broj2.Text != "" && broj3.Text != "" && broj4.Text == "")
+            {
+                broj4.Text = Convert.ToString(numbers.Next(1, 37));
+                if (broj4.Text == broj1.Text || broj4.Text == broj2.Text || broj4.Text == broj3.Text)
+                    broj4.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+            }
+            if (broj1.Text != "" && broj2.Text != "" && broj3.Text != "" && broj4.Text != "" && broj5.Text == "")
+            {
+                broj5.Text = Convert.ToString(numbers.Next(1, 37));
+                if (broj5.Text == broj1.Text || broj5.Text == broj2.Text || broj5.Text == broj3.Text || broj5.Text == broj4.Text)
+                    broj5.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+            }
+            if (broj1.Text != "" && broj2.Text != "" && broj3.Text != "" && broj4.Text != "" && broj5.Text != "" && dodatni.Text == "")
+            {
+
+                dodatni.Text = Convert.ToString(numbers.Next(1, 37));
+                if (dodatni.Text == broj1.Text || dodatni.Text == broj2.Text || dodatni.Text == broj3.Text || dodatni.Text == broj4.Text || dodatni.Text == broj5.Text)
+                    dodatni.Text = Convert.ToString(numbers.Next(1, 37));
+                return;
+            }
+            if (broj1.Text != "" && broj2.Text != "" && broj3.Text != "" && broj4.Text != "" && broj5.Text != "" && dodatni.Text != "")
+            {
+                broj.Text = broj1.Text+"   "+broj2.Text+"   "+broj3.Text+"   "+broj4.Text+"   "+ broj5.Text+"         "+ dodatni.Text;
+                return;
+            }
+
+
+      
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            broj1.Text = ""; broj2.Text = ""; broj3.Text = ""; broj4.Text = ""; broj5.Text = "";dodatni.Text = "";broj.Text = "";
+        }
+    }
+}
